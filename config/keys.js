962fdbeafd8ec0f0fb06ce1e1058690d3a2f99fc
philheb <tom@toms-MacBook-Pro.local>
2019-03-15 12:02:55 -0700

    env keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb://philippe.heb:Devjunction1!@ds127825.mlab.com:27825/webjunction',
-  secretOrKey: 'secret',
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod')
+} else {
+  module.exports = require('./keys_dev')
 }

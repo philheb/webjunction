98e7db3f87a007e06945b40eb6d8ad83d5990d83
philheb <tom@toms-MacBook-Pro.local>
2019-03-15 20:20:11 -0700

    Landing background video

--- client/src/components/Layout/Landing/Landing.js
+++ client/src/components/Layout/Landing/Landing.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
+import './Landing.css'
+import Video from '../../../assets/videos/brainstation.mp4'
 
 class Landing extends Component {
   componentDidMount() {
@@ -11,26 +13,25 @@ class Landing extends Component {
   }
   render() {
     return (
-      <div>
-        <div className="landing">
-          <div className="dark-overlay landing-inner text-light">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">Web Junction</h1>
-                  <p className="lead">
-                    {' '}
-                    Create a developer and designer profile/portfolio, share posts and get help from
-                    each other. Blah blah
-                  </p>
-                  <hr />
-                  <Link to="/register" className="btn btn-lg btn-info mr-2">
-                    Sign Up
-                  </Link>
-                  <Link to="/login" className="btn btn-lg btn-light">
-                    Login
-                  </Link>
-                </div>
+      <div className="landing">
+        <video className="landing-video" src={Video} loop autoPlay muted />
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Web Junction</h1>
+                <p className="lead">
+                  {' '}
+                  Create a developer and designer profile/portfolio, share posts and get help from
+                  each other. Blah blah
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
               </div>
             </div>
           </div>

807b552e85224821f94bd2ffc35027c208f438f3
philheb <tom@android-c9550bb7b6ec7fbc.localdomain>
2019-03-19 10:06:03 -0700

    delete experience

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -79,6 +79,24 @@ export const addEducation = (data, history) => dispatch => {
     )
 }
 
+// Delete Experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    )
+}
+
 // Delete account and profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
